17a831cdcfe5be6176efe45e5e78c0c0b27f5314
MuskAI <1553442305@qq.com>
2020-08-21 15:57:42 +0800

    21号增加划分数据集的代码

--- make_tamper_dataset_from_coco/divide_to_train_test_datasets.py
+++ make_tamper_dataset_from_coco/divide_to_train_test_datasets.py
@@ -8,8 +8,9 @@ import os
 import sys
 import datetime
 import shutil
-DATASET_SRC_PATH = 'C:\\Users\\musk\\Desktop\\test_cp\\tamper_poisson_result'
-DATASET_TARGET_PATH = 'C:\\Users\\musk\\Desktop\\test_cp\\New3'
+DATASET_SRC_PATH = 'C:\\Users\\musk\\Desktop\\test_cp\\tamper_result'
+DATASET_GT_PATH = 'C:\\Users\\musk\\Desktop\\test_cp\\ground_truth_result'
+DATASET_TARGET_PATH = 'C:\\Users\\musk\\Desktop\\test_cp\\New10'
 
 def divide(train_percent):
     if not os.path.exists(DATASET_SRC_PATH):
@@ -24,6 +25,13 @@ def divide(train_percent):
         os.mkdir(os.path.join(DATASET_TARGET_PATH, 'test_dataset_train_percent_%.2f@%d_%d'%(train_percent,
                                                                                                  datetime.datetime.now().month,
                                                                                                  datetime.datetime.now().day)))
+        os.mkdir(os.path.join(DATASET_TARGET_PATH, 'train_gt_train_percent_%.2f@%d_%d' % (train_percent,
+                                                                                              datetime.datetime.now().month,
+                                                                                              datetime.datetime.now().day)))
+        os.mkdir(os.path.join(DATASET_TARGET_PATH, 'test_gt_train_percent_%.2f@%d_%d' % (train_percent,
+                                                                                              datetime.datetime.now().month,
+                                                                                              datetime.datetime.now().day)))
+
     else:
         print('目标路径存在，检查子文件夹')
         if os.path.exists(os.path.join(DATASET_TARGET_PATH, 'train_dataset_train_percent_%d@%d_%d'.format(train_percent,
@@ -44,11 +52,30 @@ def divide(train_percent):
         shutil.copy(os.path.join(DATASET_SRC_PATH,train),os.path.join(DATASET_TARGET_PATH,'train_dataset_train_percent_%.2f@%d_%d'%(train_percent,
                                                                                                  datetime.datetime.now().month,
                                                                                                  datetime.datetime.now().day)))
-        print(index,'/',len(train_set))
+        print('train_dataset:',index,'/',len(train_set))
     for index,test in enumerate(test_set):
         shutil.copy(os.path.join(DATASET_SRC_PATH,test),os.path.join(DATASET_TARGET_PATH,'test_dataset_train_percent_%.2f@%d_%d'%(train_percent,
                                                                                                  datetime.datetime.now().month,
                                                                                                  datetime.datetime.now().day)))
+        print('test_dataset:',index, '/', len(test_set))
 
+    for index,train in enumerate(train_set):
+        train = train.replace('Default','Gt')
+        train = train.replace('poisson', 'Gt')
+        train = train.replace('png','bmp')
+        train = train.replace('jpg', 'bmp')
+        shutil.copy(os.path.join(DATASET_GT_PATH,train),os.path.join(DATASET_TARGET_PATH,'train_gt_train_percent_%.2f@%d_%d'%(train_percent,
+                                                                                                 datetime.datetime.now().month,
+                                                                                                 datetime.datetime.now().day)))
+        print('train_GT:',index,'/',len(train_set))
+    for index,test in enumerate(test_set):
+        test = test.replace('Default', 'Gt')
+        test = test.replace('poisson', 'Gt')
+        test = test.replace('png', 'bmp')
+        test = test.replace('jpg', 'bmp')
+        shutil.copy(os.path.join(DATASET_GT_PATH,test),os.path.join(DATASET_TARGET_PATH,'test_gt_train_percent_%.2f@%d_%d'%(train_percent,
+                                                                                                 datetime.datetime.now().month,
+                                                                                                 datetime.datetime.now().day)))
+        print('test_GT:',index, '/', len(test_set))
 if __name__ == '__main__':
-    divide(0.7)
+    divide(0.8)
