d72f437715b4872a07e018b0057243ded793d42c
MuskAI <1553442305@qq.com>
2020-08-28 09:09:32 +0800

    28修改的，增加的COD10K的数据生成，将多个数据数据集合并的代码

--- make_tamper_dataset_from_coco/camouflage_dataset_gen(COD10K)/COD10K.py
+++ make_tamper_dataset_from_coco/camouflage_dataset_gen(COD10K)/COD10K.py
@@ -2,6 +2,8 @@
 created by haoran
 time:8-24
 cod10k 类
+输入的是经过resize之后的图，resize的方法为双三次插值
+mask---> 篡改后的mask --->双边缘
 """
 import os,sys
 import numpy as np
@@ -12,9 +14,30 @@ import traceback
 import skimage.morphology as dilation
 import poisson_image_editing
 class COD10K():
-    def __init__(self,img_path,mask_path):
+    def __init__(self,img_path, mask_path, img_save_path):
         self.img_path = img_path
         self.mask_path = mask_path
+        self.img_save_root_path = img_save_path
+        self.img_tamper_save_path_ = os.path.join(img_save_path,'tamper_result_320_cod10k')
+        self.img_tamper_save_path = os.path.join(self.img_tamper_save_path_,'tamper_'+img_path.split('\\')[-1])
+
+        self.img_poisson_save_path_ = os.path.join(img_save_path,'tamper_poisson_result_320_cod10k')
+        self.img_poisson_save_path = os.path.join(self.img_poisson_save_path_,'tamper_poisson_'+img_path.split('\\')[-1])
+
+        self.img_gt_save_path_ = os.path.join(img_save_path,'ground_truth_result_320_cod10k')
+        self.img_gt_save_path = os.path.join(self.img_gt_save_path_,'tamper_gt_' + mask_path.split('\\')[-1])
+
+        self.tamper_num =1
+        self.bk_shape = (320,320)
+        # 检查输出文件夹
+        if os.path.exists(self.img_save_root_path):
+            print('输出文件夹已经存在，请手动更换输出文件  夹')
+        else:
+            os.mkdir(self.img_save_root_path)
+            os.mkdir(self.img_tamper_save_path_)
+            os.mkdir(self.img_poisson_save_path_)
+            os.mkdir(self.img_gt_save_path_)
+            print('输出文件夹创建成功')
 
         # 阈值
         self.area_percent_threshold = 0.5
@@ -34,18 +57,14 @@ class COD10K():
             return 'area_ok'
         pass
     def gen_data_pair(self,img_path,mask_path):
-
         tamper_img_list = []
         mask_list = []
         for t_img in os.listdir(img_path):
-            if 't' in t_img.replace('.tif',''):
-                tamper_img_list.append(os.path.join(mask_path, t_img))
-                mask_list.append(os.path.join(mask_path,t_img.replace('.tif','').replace('t','forged')+'.tif'))
-            else:
-                continue
-
+            tamper_img_list.append(os.path.join(img_path, t_img))
+            mask_list.append(os.path.join(mask_path,t_img))
         return tamper_img_list, mask_list
         pass
+
     def check_edge(self):
         pass
     def get_double_edge(self):
@@ -68,7 +87,6 @@ class COD10K():
                                                                                                            50,
                                                                                                            0)  # 所以内侧边缘就是100的灰度值
         return ground_truth
-        pass
     def random_crop(self):
         pass
     def max_save_area_crop(self):
@@ -91,27 +109,28 @@ class COD10K():
 
         return img[random_height:random_height + target_shape[0], random_width:random_width + target_shape[1]]
 
-    def random_area_to_fix_background(self,background_path, mask_path, tamper_num=1, bk_shape=(320, 320)):
+    def gen_tamper_result(self):
         """
-           输入一张背景图和一张mask图，该mask图的size不应需要与背景图一致
-           :param background:
-           :param mask: mask任意一个前景图的mask
-           :param tamper_num :篡改的数量，默认为1
-           :return: 篡改好了的图像和一张GT 是list
-           """
+        输入一对图片 src+gt
+        :return:
+        """
         # read image
-        background = Image.open(background_path)
-        mask = Image.open(mask_path)
+        background = Image.open(self.img_path)
+        background = np.array(background)
+        mask = Image.open(self.mask_path).convert('RGB')
+        mask = np.array(mask)
         mask = np.where(mask[:,:,0]==255,1,0)
+
         # 找到mask 的矩形区域
         oringal_background = background.copy()
         a = mask
         a = np.where(a != 0)
         bbox = np.min(a[0]), np.min(a[1]), np.max(a[0]), np.max(a[1])
         cut_mask = mask[bbox[0]:bbox[2], bbox[1]:bbox[3]]
+        cut_area = oringal_background[bbox[0]:bbox[2], bbox[1]:bbox[3]]
 
         # 计算物体所占区域面积
-        object_area_percent = cut_mask.size / (bk_shape[0] * bk_shape[1])
+        object_area_percent = cut_mask.size / (self.bk_shape[0] * self.bk_shape[1])
 
         # 以左上角的点作为参考点，计算可以paste的区域
         background_shape = background.shape
@@ -131,32 +150,34 @@ class COD10K():
             cut_area[:, :, 2] = cut_area[:, :, 2] * cut_mask
         else:
             # cut_area = temp_background[row1:row1 + object_area_shape[0], col1:col1 + object_area_shape[1], :]
-            cut_area = temp_background
-            cut_area[:, :, 0] = cut_area[:, :, 0] * mask
-            cut_area[:, :, 1] = cut_area[:, :, 1] * mask
-            cut_area[:, :, 2] = cut_area[:, :, 2] * mask
+            cut_area[:, :, 0] = cut_area[:, :, 0] * cut_mask
+            cut_area[:, :, 1] = cut_area[:, :, 1] * cut_mask
+            cut_area[:, :, 2] = cut_area[:, :, 2] * cut_mask
+            # plt.figure(1)
+            # plt.imshow(cut_area)
+            # plt.show()
 
-        for i in range(3):
+        for i in range(5):
             row2 = np.random.randint(0, paste_area[0])
             col2 = np.random.randint(0, paste_area[1])
-            if abs(row1 - row2) + abs(col1 - col2) < 10:
+            if abs(row1 - row2) + abs(col1 - col2) < 50:
                 print('随机选到的区域太近，最好重新选择')
             else:
                 break
 
-        # 判断object和bg的大小是否符合要求
-        if paste_area[0] < 5 or paste_area[1] < 5:
-            print('提醒：允许的粘贴区域太小')
-        if paste_area[0] < 1 or paste_area[1] < 1:
-            print('无允许粘贴的区域')
-            return False, False, False
+        # # 判断object和bg的大小是否符合要求
+        # if paste_area[0] < 5 or paste_area[1] < 5:
+        #     print('提醒：允许的粘贴区域太小')
+        # if paste_area[0] < 1 or paste_area[1] < 1:
+        #     print('无允许粘贴的区域')
+        #     return False, False, False
         # 随机在background上贴上该mask的区域，并且保证与原区域有一定的像素偏移,然后生成新的mask图
 
         tamper_image = []
         tamper_mask = []
         tamper_gt = []
         tamper_poisson = []
-        for times in range(tamper_num):
+        for times in range(self.tamper_num):
             bk_mask = np.zeros((background_shape[0], background_shape[1]), dtype='uint8')
             bk_area = np.zeros((background_shape[0], background_shape[1], 3), dtype='uint8')
             bk_mask[row2:row2 + object_area_shape[0], col2:col2 + object_area_shape[1]] = cut_mask
@@ -198,33 +219,49 @@ class COD10K():
             background = Image.fromarray(oringal_background)
             mask = bk_mask
             try:
-                poisson_foreground = cv.cvtColor(np.asarray(foreground.convert('RGB')), cv.COLOR_RGB2BGR)
-                poisson_background = cv.cvtColor(np.asarray(background), cv.COLOR_RGB2BGR)
-                poisson_mask = np.asarray(mask)
-                poisson_mask = np.where(poisson_mask == 1, 255, 0)
-                poisson_fusion_image = poisson_image_editing.poisson_fusion(poisson_foreground, poisson_background,
-                                                                            poisson_mask)
-                poisson_fusion_image = Image.fromarray(cv.cvtColor(poisson_fusion_image, cv.COLOR_BGR2RGB))
-
+                # poisson_foreground = cv.cvtColor(np.asarray(foreground.convert('RGB')), cv.COLOR_RGB2BGR)
+                # poisson_background = cv.cvtColor(np.asarray(background), cv.COLOR_RGB2BGR)
+                # poisson_mask = np.asarray(mask)
+                # poisson_mask = np.where(poisson_mask == 1, 255, 0)
+                # poisson_fusion_image = poisson_image_editing.poisson_fusion(poisson_foreground, poisson_background,
+                #                                                             poisson_mask)
+                # poisson_fusion_image = Image.fromarray(cv.cvtColor(poisson_fusion_image, cv.COLOR_BGR2RGB))
                 #
-                # plt.figure('123')
-                # plt.imshow(poisson_fusion_image)
-                # plt.show()
-                tamper_poisson.append(poisson_fusion_image)
-                # return tamper_image, tamper_poisson, tamper_gt
+                # #
+                # # plt.figure('123')
+                # # plt.imshow(poisson_fusion_image)
+                # # plt.show()
+                # tamper_poisson.append(poisson_fusion_image)
+                # # return tamper_image, tamper_poisson, tamper_gt
 
                 # 保存
+                for index,t_img in enumerate(tamper_image):
+                    t_img = Image.fromarray(t_img)
+                    t_img.save(self.img_tamper_save_path)
+                    # t_img.save(os.path.join(self.img_tamper_save_path,t_img.split('\\')[-1]))
 
+                for index, t_gt in enumerate(tamper_gt):
+                    t_img = Image.fromarray(t_gt).convert('RGB')
+                    t_img.save(self.img_gt_save_path)
+                    pass
             except Exception as e:
                 traceback.print_exc()
 
 
 if __name__ == '__main__':
-    img_path = ''
-    mask_path = 'H:\COD10K_resize\\\GT'
-    for mask in os.listdir(mask_path):
-        gt_path = os.path.join(mask_path,mask)
-        gt = COD10K(img_path,gt_path).get_double_edge()
-        plt.figure(1)
-        plt.imshow(gt)
-        plt.show()
+    img_path = 'H:\\COD10K_resize\\src'
+    mask_path = 'H:\\COD10K_resize\\GT'
+    img_save_root_path = 'H:\\COD10K_resize\\save3'
+    img_path_list = []
+    mask_path_list = []
+    for index,t_img_path in enumerate(os.listdir(img_path)):
+        img_path_list.append(os.path.join(img_path,t_img_path))
+        img_path_ = os.path.join(img_path,t_img_path)
+        mask_path_list.append(os.path.join(mask_path, t_img_path.replace('png','bmp')))
+
+    for i in range(len(img_path_list)):
+        print(i)
+        print(img_path_list[i])
+        print(mask_path_list[i])
+        print(img_save_root_path)
+        COD10K(img_path_list[i],mask_path_list[i],img_save_root_path).gen_tamper_result()
